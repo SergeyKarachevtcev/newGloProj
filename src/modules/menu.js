821c4f8f66efa345a01537c30529d54a83be4ce1
SergeyKarachevtcev <estel141081@gmail.com>
2024-02-16 13:48:52 +0300

    проверка 22

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -4,8 +4,7 @@ const menu = () => {
 	const menuBtn = document.querySelector(".menu");
 	const menu = document.querySelector("menu");
 	const closeBtn = menu.querySelector(".close-btn");
-	const menuItems = menu.querySelectorAll("ul>li>a");
-
+	
 	const handleMenu = (event) => {
 		if (
 			event.target.classList.contains("menu") ||
