b33990be4a8c8990df8322c3dd8df1e1a4064d08
SergeyKarachevtcev <estel141081@gmail.com>
2024-02-16 16:01:08 +0300

    поправил домашку

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -4,14 +4,13 @@ const menu = () => {
 	const menuBtn = document.querySelector(".menu");
 	const menu = document.querySelector("menu");
 	const closeBtn = menu.querySelector(".close-btn");
-
 	const handleMenu = (event) => {
 		if (
 			menuBtn ||
 			event.target.classList.contains("menu") ||
 			event.target === closeBtn
 		) {
-			menu.classList.toggle("active-menu");
+			menu.classList.toggle("active-menu"); // добавляю или отменяю класс у кнопки меню , кнопки закрытия
 		} else if (event.target.closest("ul>li>a")) {
 			menu.classList.remove("active-menu");
 		}
